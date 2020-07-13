bce0cb1208aaea4f2a42f3c5d202ec96949ee52f
josh <joshua.sandefer@gmail.com>
2020-07-13 14:52:27 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MenuPageComponent } from './menu-page/menu-page.component';
+import { MenuItemComponent } from './menu-page/menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainPageComponent,
+    MenuPageComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
